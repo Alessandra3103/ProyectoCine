de42e26804e3c7e139c5e1cf2f66875b1b6474bd
adrian-garcia-cuya <adrianisa123575@outlook.com>
2023-04-09 18:47:15 -0500

    Initial commit

---
+++ app/src/main/java/com/project/moviebag/utils/Credentials.java
@@ -0,0 +1,10 @@
+package com.project.moviebag.utils;
+
+import com.project.moviebag.BuildConfig;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+
+    public static final String API_KEY = "";
+}
